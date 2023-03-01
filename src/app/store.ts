6311a4613152bcd4c9a5bc04ec21754e07fc3377
Pazhiloi <martinsen06@gmail.com>
2023-03-01 10:30:07 +0200

    added redux-persist as a localStorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,28 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from '../features/user/userSlice';
 import newsReducer from "../features/news/newsSlice";
 
-export const store = configureStore({
-  reducer: {
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     user: userReducer,
     news: newsReducer,
-  },
+  })
+);
+
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
